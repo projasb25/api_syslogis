ae24263785e67b5f9ebafa024bc03228a4ef6968
Pablo Rojas <pablo.rojas@latinfintech.com>
2020-09-28 17:20:28 -0500

    exception

--- app/Models/Repositories/Web/MainRepository.php
+++ app/Models/Repositories/Web/MainRepository.php
@@ -2,12 +2,17 @@
 
 namespace App\Models\Repositories\Web;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 
 class MainRepository
 {
     public function execute_store($sp_name, $data_bidnings)
     {
-        return DB::select($sp_name,$data_bidnings);
+        try {
+            return DB::select($sp_name,$data_bidnings);
+        } catch (QueryException $th) {
+            throw $th;
+        }
     }
 }
