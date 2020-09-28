f87d0f3c167e7c5996babad3f436bf6f27b0591d
Pablo Rojas <pablo.rojas@latinfintech.com>
2020-09-28 17:27:47 -0500

    test

--- app/Models/Repositories/Web/MainRepository.php
+++ app/Models/Repositories/Web/MainRepository.php
@@ -11,9 +11,9 @@ class MainRepository
     public function execute_store($sp_name, $data_bidnings)
     {
         try {
+            Log::info('test'.$sp_name, $data_bidnings);
             return DB::select($sp_name,$data_bidnings);
         } catch (QueryException $th) {
-            Log::info('entras aca?');
             throw $th;
         }
     }
