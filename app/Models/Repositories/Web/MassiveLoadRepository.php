664912ade18554ca5d3a44b415e37991c9d87f2a
Pablo Rojas <pablo.rojas@latinfintech.com>
2020-10-11 22:45:10 -0500

    hoja_ruta

--- app/Models/Repositories/Web/MassiveLoadRepository.php
+++ app/Models/Repositories/Web/MassiveLoadRepository.php
@@ -208,8 +208,8 @@ class MassiveLoadRepository
                 ]);
 
                 if ($value->sku_code === NULL || !isset($value->sku_code)) {
-                    $sku_code =  'SKU' . str_pad($id_sku, 7, "0", STR_PAD_LEFT);
-                    DB::table('sku_product')->where('id_sku_product', $id_sku)->update(['sku_code' => $sku_code]);
+                    $v_sku_code =  'SKU' . str_pad($id_sku, 7, "0", STR_PAD_LEFT);
+                    DB::table('sku_product')->where('id_sku_product', $id_sku)->update(['sku_code' => $v_sku_code]);
                 }
 
                 $total_weight =+ $value->sku_weight;
