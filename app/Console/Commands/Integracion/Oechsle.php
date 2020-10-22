0a6cb1c46317a317545e89a32e49c66a17b4b9e8
Pablo Rojas <pablo.rojas@latinfintech.com>
2020-10-21 22:40:22 -0500

    integracion oeschle

---
+++ app/Console/Commands/Integracion/Oechsle.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands\Integracion;
+
+use Exception;
+use Illuminate\Console\Command;
+
+class Oechsle extends Command
+{
+    protected $integracionServi;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'integracion:oechsle';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Integracion Oechsle';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(IntegracionService $integracionService)
+    {
+        $this->integracionServi = $integracionService;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->line("INTEGRACION CON RIPLEY");
+            $this->line("=============================================");
+            $this->line('');
+
+            $integracion = $this->integracionServi->integracionRipley();
+            $integracion = $this->integracionServi->integracionOechsle();
+            if (!$integracion['success']) {
+                throw new Exception($integracion['mensaje'], 500);
+            }
+
+            $this->info('Integracion con ripley');
+            $this->info('');
+
+        } catch (Exception $exc) {
+            $this->error($exc->getMessage());
+        }
+    }
+}
