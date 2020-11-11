e5b27fc677174e46c9701a01fe109ae203f72527
Pablo Rojas <pablo.rojas@latinfintech.com>
2020-11-10 21:05:51 -0500

    hoja cargo

--- app/Models/Services/Web/MassiveLoadService.php
+++ app/Models/Services/Web/MassiveLoadService.php
@@ -214,7 +214,7 @@ class MassiveLoadService
                     $pdf->SetXY($box_x+8, $box_y + 1);
                     $pdf->MultiCell(89,4,'NOMBRE: '. $guide->name,0,'J');
                     $pdf->SetX($box_x+8);
-                    $pdf->MultiCell(89,4,'DIST.: ' . $guide->district,0,'J');
+                    $pdf->MultiCell(89,4,'CIUDAD: LIMA',0,'J');
                     $pdf->SetX($box_x+8);
                     $pdf->MultiCell(89,4,'FECHA: '. Carbon::createFromFormat('Y-m-d H:i:s', $guide->date_created)->format('Y-m-d'),0,'J');
                     $pdf->SetX($box_x+8);
@@ -280,7 +280,7 @@ class MassiveLoadService
                     $pdf->SetXY($box_x + 99 + 7, $box_y + 1);
                     $pdf->MultiCell(89,4,'NOMBRE: '. $guide->client_name,0,'J');
                     $pdf->SetX($box_x + 99 + 7);
-                    $pdf->MultiCell(89,4,'CIUDAD: '. $guide->province,0,'J');
+                    $pdf->MultiCell(89,4,'DIST.: ' . $guide->district,0,'J');
                     $pdf->SetX($box_x + 99 + 7);
                     $pdf->Cell(27,4,'TELEFONO: '. $guide->client_phone1 ,0,0,'L');
                     $pdf->Cell(52,4,'EMAIL: '. utf8_decode(strtolower($guide->client_email)),0,1,'L'); //lower to space
