5bd0d3e2cf0e1d3793cab23dba463b825d93595f
Pablo Rojas <pablo.rojas@latinfintech.com>
2021-02-11 22:43:50 -0500

    load_templaate

--- app/Models/Functions/FunctionModel.php
+++ app/Models/Functions/FunctionModel.php
@@ -36,9 +36,9 @@ class FunctionModel
             "query" => 'CALL SP_SEL_CLIENT_STORE(:id_client, :status)',
             "params" => ['id_client', 'status']
         ],
-        "SP_INS_BILL_TEMPLATE" => [
-            "query" => 'CALL SP_INS_BILL_TEMPLATE(:id_bill_load_template,:id_corporation,:id_organization,:id_client,:name,:description,:json_detail,:status,:username)',
-            "params" => ['id_bill_load_template','id_corporation','id_organization','id_client','name','description','json_detail','status','username']
+        "SP_INS_LOAD_TEMPLATE" => [
+            "query" => 'CALL SP_INS_LOAD_TEMPLATE(:id_bill_load_template,:id_corporation,:id_organization,:id_client,:name,:description,:json_detail,:status,:username, :type)',
+            "params" => ['id_bill_load_template','id_corporation','id_organization','id_client','name','description','json_detail','status','username','type']
         ],
         "SP_SEL_BILL_TEMPLATE" => [
             "query" => 'CALL SP_SEL_BILL_TEMPLATE(:id_corporation, :id_organization, :status)',
