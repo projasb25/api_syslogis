c7d1bf124a58e7409ca7a934567ef37e6d0dd807
Pablo Rojas <projasb25@gmail.com>
2021-02-28 13:55:31 -0500

    discont_from

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,8 @@ use Location\Distance\Vincenty;
 
 Route::post('test', function(Request $request){
     $inventario = DB::table('inventory')->where('id_product',124)->where('available','>',0)->first();
-    $property_name = 'shrinkage';
+    $property_name = 'column';
+    DB::table('inventory')->where('id_inventory',160)->update([$property_name => 20]);
     dd($inventario->$property_name);
 });
 
