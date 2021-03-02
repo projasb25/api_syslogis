7b4a07ab8782386d35b9f49230abb1625d3b7a54
Pablo Rojas <projasb25@gmail.com>
2021-03-01 20:00:31 -0500

    reporte

--- resources/views/pdf/orden_compra/detalle.blade.php
+++ resources/views/pdf/orden_compra/detalle.blade.php
@@ -94,22 +94,17 @@
                 </thead>
                 <tbody>
                     @foreach ($purchase_order as $key => $item)
-                    {{
-                        if($item->shrinkage > 0){
-                            $origen = "Merma";
-                        } elseif($item->quarantine > 0){
-                            $origen = "Cuarentena";
-                        }
-                        elseif($item->scrap > 0){
-                            $origen = "Scrap";
-                        }
-                        elseif($item->demo > 0){
-                            $origen = "Demo";
-                        }
-                        else {
-                            $origen = "Disponible";
-                        }
-                    }}
+                        @if ($item->shrinkage > 0)
+                            {{$origen = "Merma"}}
+                        @elseif ($item->quarantine > 0)
+                            {{$origen = "Cuarentena"}}
+                        @elseif ($item->scrap > 0)
+                            {{$origen = "Scrap"}}
+                        @elseif ($item->demo > 0)
+                            {{$origen = "Demo"}}
+                        @else
+                            {{$origen = "Disponible"}}
+                        @endif
                     <tr>
                         <td>{{$key+1}}</td>
                         <td style="text-align:left;padding-left: 10px;">{{$item->product_code}}</td>
