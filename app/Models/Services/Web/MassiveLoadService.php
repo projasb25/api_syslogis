ab251ae65080db317f80b09f83b5e25bcd529083
Pablo Rojas <projasb25@gmail.com>
2021-03-20 16:44:43 -0500

    si existe archivo

--- app/Models/Services/Web/MassiveLoadService.php
+++ app/Models/Services/Web/MassiveLoadService.php
@@ -191,7 +191,9 @@ class MassiveLoadService
         $disk = Storage::disk('cargo');
         $ruta = url('storage/cargo/');
 
-        if (!$massive_load->ruta_doc_cargo) {
+        $file_exists = (Storage::disk('cargo')->exists($massive_load->ruta_doc_cargo));
+
+        if (!$massive_load->ruta_doc_cargo || !$file_exists) {
             if ($massive_load->id_corporation === 4) {
                 $data = $this->repo->get_datos_ruta_cargo_oechsle($massive_load->id_massive_load);
                 $motivos = $this->repo->get_motivos();
