25d08595de07b279d8007e22cdccd78eba5217b9
Pablo Rojas <projasb25@gmail.com>
2021-03-21 16:08:44 -0500

    proecas carga

--- app/Models/Services/Web/MassiveLoadService.php
+++ app/Models/Services/Web/MassiveLoadService.php
@@ -310,13 +310,13 @@ class MassiveLoadService
                     $pdf->SetX($box_x + 92 + 7);
                     $pdf->MultiCell(101,5,'TLF.: ' . $guide->client_phone1,0,'J');
                     $pdf->SetX($box_x + 92 + 7);
-                    $pdf->MultiCell(101,5,'EMAIL.: ' .utf8_decode(strtolower($guide->client_email)),0,'J');
+                    $pdf->MultiCell(101,5,'CONTACTO: ' .utf8_decode(strtolower($guide->collect_contact_name)),0,'J');
                     $pdf->SetX($box_x + 92 + 7);
                     $pdf->MultiCell(101,5,'REF: ' .utf8_decode(strtolower($guide->client_email)),0,'J');
                     $pdf->SetX($box_x + 92 + 7);
-                    $pdf->MultiCell(101,5,'FECHA REC.: ' .utf8_decode(strtolower($guide->client_email)),0,'J');
+                    $pdf->MultiCell(101,5,'FECHA REC.: ' .utf8_decode(strtolower($guide->client_date)),0,'J');
                     $pdf->SetX($box_x + 92 + 7);
-                    $pdf->MultiCell(101,5,'HORARIO REC.: ' .utf8_decode(strtolower($guide->client_email)),0,'J');
+                    $pdf->MultiCell(101,5,'HORARIO REC.: ' .utf8_decode(strtolower($guide->collect_time_range)),0,'J');
                     $pdf->SetX($box_x + 92 + 7);
                     $pdf->SetFont('Times', 'B', 10);
                     $pdf->MultiCell(100,5,'DIRECCION: '. $direccion,0,'L');
