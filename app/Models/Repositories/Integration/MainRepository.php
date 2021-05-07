e6cb69a8778a83cebcb27c9a357c6b256d1e9492
Pablo Rojas <projasb25@gmail.com>
2021-05-07 13:36:35 -0500

    nueva estructura

--- app/Models/Repositories/Integration/MainRepository.php
+++ app/Models/Repositories/Integration/MainRepository.php
@@ -23,46 +23,45 @@ class MainRepository
 
             $idOriginal = 'RP' . Carbon::now()->format('Ymd') . str_pad($id, 6, "0", STR_PAD_LEFT);
 
-            foreach ($data['DetalleProductos'] as $value) {
-                $ubigeo_recoleccion = DB::table('ubigeo')->where('ubigeo', $data['UbigeoOrigen'])->first();
-                $ubigeo_dist = DB::table('ubigeo')->where('ubigeo', $data['UbigeoDestino'])->first();
-                
+            foreach ($data['items'] as $value) {
+                $ubigeo_recoleccion = DB::table('ubigeo')->where('ubigeo', $data['sellerUbigeo'])->first();
+                $ubigeo_dist = DB::table('ubigeo')->where('ubigeo', $data['clientUbigeo'])->first();
+
                 DB::table('integration_data_detail')->insert(
                     [
                         'id_integration_data' => $id,
                         'status' => 'PENDIENTE',
                         'created_by' => $user->integration_user,
-                        'seg_code' => $data['CodCliente'],
+                        'seg_code' => $data['orderNumber'],
                         'guide_number' => $idOriginal,
                         'client_barcode' => $idOriginal,
-                        'sku_code' => $value['codigoProducto'],
-                        'sku_description' => $value['descripcionProducto'],
-                        'sku_weight' => $value['pesoProducto'],
-                        'sku_pieces' => $value['CantidadProducto'],
+                        'sku_code' => $value['id'],
+                        'sku_description' => $value['description'],
+                        'sku_weight' => $value['weight'],
+                        'sku_pieces' => $value['quantity'],
                         'collect_department' => $ubigeo_recoleccion->department,
                         'collect_district' => $ubigeo_recoleccion->district,
                         'collect_province' => $ubigeo_recoleccion->province,
-                        'collect_address_reference' => $data['DireccionReferencialRemitente'],
-                        'collect_address' => $data['DireccionRemitente'],
-                        'collect_client_dni' => $data['NroDocumentoRemitente'],
-                        'collect_client_name' => $data['NombreRemitente'],
-                        'collect_client_phone1' => $data['TelefonoRemitente'],
-                        'collect_contact_name' => $data['ContactoRemitente'],
-                        'collect_client_email' => $data['CorreoRemitente'],
+                        'collect_address_reference' => '',
+                        'collect_address' => $data['sellerAddress'],
+                        'collect_client_dni' => $data['sellerCorporateDocument'],
+                        'collect_client_name' => $data['sellerCorporateName'],
+                        'collect_client_phone1' => $data['sellerPhone'],
+                        'collect_contact_name' => '',
+                        'collect_client_email' => $data['sellerEmail'],
                         'delivery_department' => $ubigeo_dist->department,
                         'delivery_district' => $ubigeo_dist->district,
                         'delivery_province' => $ubigeo_dist->province,
-                        'delivery_address_reference' => $data['DireccionReferencialDestinatario'],
-                        'delivery_address' => $data['DireccionDestinatario'],
-                        'delivery_client_dni' => $data['NroDocumentoDestinatario'],
-                        'delivery_client_name' => $data['NombreDestinatario'],
-                        'delivery_client_phone1' => $data['TelefonoDestinatario'],
-                        'delivery_contact_name' => $data['ContactoDestinatario'],
-                        'delivery_contact_email' => $data['CorreoDestinatario']
+                        'delivery_address_reference' => $data['clientAddressReference'],
+                        'delivery_address' => $data['clientAddressStreet'],
+                        'delivery_client_dni' => $data['clientDocument'],
+                        'delivery_client_name' => $data['clientFirstName']. ' '. $data['clientLastName'],
+                        'delivery_client_phone1' => $data['clientPhone'],
+                        'delivery_contact_name' => $data['clientFirstName']. ' '. $data['clientLastName'],
+                        'delivery_contact_email' => $data['clientEmail']
                     ]
                 );
             }
-
         } catch (Exception $e) {
             Log::warning("Actualizar estado conductor " . $e->getMessage());
             DB::rollback();
