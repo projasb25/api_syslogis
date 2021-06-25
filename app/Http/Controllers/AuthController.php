8822d5ec6db773074e747ff77f160b0949b793e7
Pablo Rojas <projasb25@gmail.com>
2021-06-24 21:02:40 -0500

    DB

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
     public function __construct(ConductorRepository $conductorRepository)
     {
         $this->conductorRepo = $conductorRepository;
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     /**
@@ -42,6 +42,67 @@ class AuthController extends Controller
             if (!Hash::check($data['password'], $query[0]->password)) {
                 throw new CustomException(['Credenciales incorrectas.', 2001], 401);
             }
+            $user = User::where('id_user', $query[0]->id_user)->first();
+
+            // if ($user->id_user === 1) {
+            //     $corporaciones = DB::select('CALL SP_SEL_CORPORATIONS(?,?)', [null, null]);
+            //     $organizaciones = DB::select('CALL SP_SEL_ORGANIZATIONS(?,?)', [null, $query[0]->current_corp]);
+            // } else {
+            //     $organizaciones = DB::select('CALL SP_SEL_ORGUSER(?,?)', [null, $user->id_user]);
+            //     $corporaciones = null;
+            // }
+
+            // $token = auth()->claims(
+            //         ['current_org' => $query[0]->current_org, 'current_corp' => $query[0]->current_corp]
+            //     )->login($user);
+            $token = auth()->login($user);
+
+            return Res::success([
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'type' => $user->type,
+                'token' => $token,
+                // 'rol_name' => $query[0]->role_name,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ]);
+
+        } catch (CustomException $e) {
+            Log::warning('Iniciar Session error', ['expcetion' => $e->getData()[0], 'request' => request()->all()]);
+            return Res::error($e->getData(), $e->getCode());
+        } catch (Exception $e) {
+            Log::warning('Iniciar Session error', ['exception' => $e->getMessage(), 'request' => request()->all()]);
+            return Res::error(['Unxpected error', 3000], 400);
+        }
+    }
+
+    public function register()
+    {
+        try {
+            Config::set('auth.providers.users.model', \App\User::class);
+
+            $data = request()->get('data');
+            $data['password'] = Hash::make($data['password']);
+            $query = DB::select("CALL SP_INS_USER(?,?,?,?,?,?,?,?,?,?,?)", [
+                $data['id_user'],
+                $data['username'],
+                $data['first_name'],
+                $data['last_name'],
+                $data['doc_type'],
+                $data['doc_number'],
+                $data['user_email'],
+                $data['password'],
+                $data['status'],
+                $data['type'],
+                'APP',
+            ]);
+
+            // if (!$query) {
+            //     throw new CustomException(['Usuario no existe.', 2000], 401);
+            // }
+            // if (!Hash::check($data['password'], $query[0]->password)) {
+            //     throw new CustomException(['Credenciales incorrectas.', 2001], 401);
+            // }
             $user = User::where('id_user', $query[0]->id_user)->first();
 
             // if ($user->id_user === 1) {
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
     Route::get('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
