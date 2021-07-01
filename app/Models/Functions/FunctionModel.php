0419f61652232dad2f5ea0002c6d4ac68d2029e9
Daniel Chunga <edchunga10@gmail.com>
2021-06-30 19:33:10 -0500

    Agregando 4 SP

--- app/Models/Functions/FunctionModel.php
+++ app/Models/Functions/FunctionModel.php
@@ -29,6 +29,22 @@ class FunctionModel
             "params" => ['id_order','id_user']
         ],
         
+        "SP_SEL_ORDER_STATUS" => [
+            "query" => 'CALL SP_SEL_ORDER_STATUS()',
+            "params" => []
+        ],
+        "SP_ASIG_ORDER" => [
+            "query" => 'CALL SP_ASIG_ORDER(:id_order, :id_driver)',
+            "params" => ['id_order', 'id_driver']
+        ],
+        "SP_CHANGE_ORDER_STATUS" => [
+            "query" => 'CALL SP_CHANGE_ORDER_STATUS(:id_order, :status)',
+            "params" => ['id_order', 'status']
+        ],
+        "SP_LIS_DRIVER_ORDER" => [
+            "query" => 'CALL SP_LIS_DRIVER_ORDER(:id_driver)',
+            "params" => ['id_driver']
+        ],
         /**
          * Funciones para Transaccions
          **/
@@ -48,7 +64,8 @@ class FunctionModel
         "SP_SEL_CLIENT_ORDER_COUNT" => [
             "query" => 'CALL SP_SEL_CLIENT_ORDER_COUNT(?,?)',
             "params" => []
-        ],
+        ]
+        
     ];
 
     public function getFunctions()
