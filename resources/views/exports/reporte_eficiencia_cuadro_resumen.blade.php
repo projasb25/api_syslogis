0a006c74d83b3afd21504024f862257811db0d2b
Pablo Rojas <projasb25@gmail.com>
2021-07-05 16:38:00 -0500

    test fechas

--- resources/views/exports/reporte_eficiencia_cuadro_resumen.blade.php
+++ resources/views/exports/reporte_eficiencia_cuadro_resumen.blade.php
@@ -24,7 +24,7 @@
             <td>{{$data->total_suma}}</td>
         </tr>
         <tr>
-            <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ENTREGADO</td>
+            <td><pre>     ENTREGADO</pre></td>
         </tr>
         @endforeach
     </tbody>
