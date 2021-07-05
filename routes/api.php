7fe911a3cd4487059c62dbb48d112b3d261e3605
Pablo Rojas <projasb25@gmail.com>
2021-07-05 15:39:08 -0500

    test fechas

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Location\Distance\Vincenty;
 */
 
 Route::post('test', function(){
-    $cuadro_detalle = DB::select("CALL SP_REP_EFICIENCIA_V2_PT1(?,?,?,?,?,'RECOLECCION')",[1, 30, '2021-07-01', '2021-07-03', 'rpjas']);
+    $cuadro_resumen = DB::select("CALL SP_REP_EFICIENCIA_V2_PT1(?,?,?,?,?,'RECOLECCION')",[1, 30, '2021-07-01', '2021-07-03', 'rpjas']);
     dd($cuadro_resumen);
 });
 
