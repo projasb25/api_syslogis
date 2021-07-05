61ca457abaf4599e75bce127c719536e80fe8689
Pablo Rojas <projasb25@gmail.com>
2021-07-05 16:10:33 -0500

    test fechas

--- resources/views/exports/reporte_eficiencia_cuadro_resumen.blade.php
+++ resources/views/exports/reporte_eficiencia_cuadro_resumen.blade.php
@@ -21,7 +21,7 @@
         <tr>
             <td>{{$data->fecha}}</td>
             @foreach ($fechas as $item)
-                {{test()}}
+                {!! test() !!}
                 {{-- @foreach ($cuadro_detalle as $val)
                     <td>
                         @if ($val->fecha_promesa === $data->fecha && $val->fecha_entrega === $item)
