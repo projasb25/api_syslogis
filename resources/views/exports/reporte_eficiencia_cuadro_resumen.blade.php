2e33e85dd5d7551c2f2fa6f8b448d22e03b57e56
Pablo Rojas <projasb25@gmail.com>
2021-07-05 15:31:25 -0500

    test fechas

--- resources/views/exports/reporte_eficiencia_cuadro_resumen.blade.php
+++ resources/views/exports/reporte_eficiencia_cuadro_resumen.blade.php
@@ -1,21 +1,17 @@
-<pre>
-    @php
-                var_dump($cuadro_detalle);
-    @endphp
-</pre>
-{{-- <table>
+@php
+    $fechas = array_unique(array_column($cuadro_detalle, 'fecha_entrega'));
+    sort($fechas);
+@endphp
+<table>
     <thead>
         <tr>
             <th>Etiquetas de fila</th>
-            <th style="color:white;background-color:#bf000b;border: 1px solid black;">FECHA RECEPCION DATA</th>
-            <th style="color:white;background-color:#bf000b;border: 1px solid black;">ENTREGADO</th>
-            <th style="color:white;background-color:#bf000b;border: 1px solid black;">NO ENTREGADO</th>
-            <th style="color:white;background-color:#bf000b;border: 1px solid black;">Suma total</th>
-            <th style="color:white;background-color:#bf000b;border: 1px solid black;">Efectividad</th>
-            <th style="color:white;background-color:#bf000b;border: 1px solid black;">Eficiencia</th>
+            @foreach ($fechas as $item)
+                <th style="color:white;background-color:#bf000b;border: 1px solid black;">{{$item}}</th>
+            @endforeach
         </tr>
     </thead>
     <tbody>
-        {{-- {{dd($cuadro_detalle)}}
+
     </tbody>
-</table> --}}
+</table>
