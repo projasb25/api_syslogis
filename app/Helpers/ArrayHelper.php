d700e1c57f6e5f7c40fa55d8a0b724a0cc722b1e
Pablo Rojas <projasb25@gmail.com>
2021-07-05 16:11:54 -0500

    test fechas

--- app/Helpers/ArrayHelper.php
+++ app/Helpers/ArrayHelper.php
@@ -7,4 +7,9 @@ class ArrayHelper
     public static function array_keys_exists(array $keys, array $arr) {
         return !array_diff_key(array_flip($keys), $arr);
     }
+
+    public static function test()
+    {
+        return 'test';
+    }
 }
