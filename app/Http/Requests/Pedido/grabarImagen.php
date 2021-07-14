231a2f71ed581de125d6b76bec89e6a38eaa0146
Pablo Rojas <projasb25@gmail.com>
2021-07-13 22:26:16 -0500

    test

--- app/Http/Requests/Pedido/grabarImagen.php
+++ app/Http/Requests/Pedido/grabarImagen.php
@@ -21,7 +21,6 @@ class grabarImagen extends FormRequest
         return [
             'imagen' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:20048',
             'id_order' => 'required|numeric',
-            'guide_number' => 'required|string',
             // 'id_shipping_order_detail' => 'required|numeric',
             'tipo_imagen' => 'required',
             'descripcion' => 'required|string'
@@ -35,7 +34,6 @@ class grabarImagen extends FormRequest
             'imagen.mimes'  => 'Extension invalida.',
             'id_order.required' => 'Falta id_order',
             'id_order.numeric' => 'pedido invalido',
-            'guide_number.*' => 'pedido invalido',
             // 'id_shipping_order_detail.required' => 'Falta id_shipping_order_detail',
             // 'id_shipping_order_detail.numeric' => 'pedido invalido',
             'tipo_imagen.*' => 'Tipo imagen inválido.',
