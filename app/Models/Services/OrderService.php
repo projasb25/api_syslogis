f53d55eb88d9f8709b5dd13196054cbdf4a8ea7c
Pablo Rojas <projasb25@gmail.com>
2021-08-17 01:30:42 -0500

    avances

--- app/Models/Services/OrderService.php
+++ app/Models/Services/OrderService.php
@@ -75,7 +75,7 @@ class OrderService
             $id = $this->repository->createOrder($header, $detail, $user_data);
         } catch (CustomException $e) {
             Log::warning('Insert order error', ['expcetion' => $e->getData()[0], 'request' => $data]);
-            dd($e);
+            dd($e->getData());
             return Res::error( $e->getData()[0], $e->getCode());
         } catch (QueryException $e) {
             Log::warning('Insert order query error', ['expcetion' => $e->getMessage(), 'request' => $data]);
