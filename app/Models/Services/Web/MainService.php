3fb1359e273bcb26c139bdbdf90d9c962d17049a
Pablo Rojas <projasb25@gmail.com>
2021-10-01 23:40:17 -0500

    cambios

--- app/Models/Services/Web/MainService.php
+++ app/Models/Services/Web/MainService.php
@@ -48,6 +48,7 @@ class MainService
 
             // Si existe un id_user en params, no usar el de la sesion
             if (in_array('id_user', $params) && array_key_exists('id_user', $fields)) {
+                dd('aca');
                 $fields['id_user'] = $req['data']['id_user'];
             }
 
