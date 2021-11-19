e01c19d5ea88bbfdcc083f77bdf6ef909aee31aa
Pablo Rojas <projasb25@gmail.com>
2021-11-18 22:20:07 -0500

    nuevo proceso

---
+++ app/Console/Commands/Procesos/Integracion/ProcesoRecoleccion.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands\Procesos\Integracion;
+
+use App\Models\Services\Integration\MainService;
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class ProcesoRecoleccion extends Command
+{
+    protected $mainService;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'procesos:recoleccion';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Procesar carga a recoleccion de clientes de integracion';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(MainService $mainService)
+    {
+        $this->mainService = $mainService;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // try {
+        //     $this->line("PROCESAR CARGA INTEGRACION - RECOLECCION");
+        //     $this->line("=============================================");
+        //     $this->line('');
+
+        //     $integracion = $this->mainService->procesar_recoleccion();
+        //     if (!$integracion['success']) {
+        //         throw new Exception($integracion['mensaje'], 500);
+        //     }
+
+        //     $this->info('IN RETAIL PROCESADO CON EXITO - RECOLECCION');
+        //     $this->info('');
+
+        // } catch (Exception $exc) {
+        //     $this->error($exc->getMessage());
+        // }
+    }
+}
