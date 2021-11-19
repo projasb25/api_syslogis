c0785bc0963df173f84cf0dc7d9649831f2ff414
Pablo Rojas <projasb25@gmail.com>
2021-11-18 22:45:55 -0500

    pruebas

--- app/Models/Services/Web/ShippingService.php
+++ app/Models/Services/Web/ShippingService.php
@@ -185,7 +185,7 @@ class ShippingService
     
             $pdf->SetDrawColor(150, 153, 141);
             $pdf->Line(10, 41, 195, 41);
-            $pdf->Ln(0);
+            $pdf->Ln(2);
             $y = $pdf->GetY();
     
             // total largo pagina 210
