ae7d0d9a14362d45ec64a3f29d81d955559a5870
Pablo Rojas <projasb25@gmail.com>
2022-02-18 11:34:37 -0500

    log

--- app/Models/Services/IntegracionService.php
+++ app/Models/Services/IntegracionService.php
@@ -226,6 +226,7 @@ class IntegracionService
                     } catch (\GuzzleHttp\Exception\RequestException $e) {
                         Log::error('exception', ['exc' => $e->getResponse()->getBody()->getContents()]);
                         $response = $e->getResponse()->getBody()->getContents();
+                        Log::error('tasdfasd', ['response' => $response]);
                         Log::error('Reportar estado a Oechsle, ', ['req' => $req_body, 'exception' => $response]);
                         $this->repository->LogInsertOechsle_inter('ERROR', $req_body, $response, $guias, $guide->alt_code1, $guide->status, $type);
                         $this->repository->updateReportadoOeschle($guias, 2);
