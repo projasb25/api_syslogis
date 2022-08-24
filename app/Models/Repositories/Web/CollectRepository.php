6b08cfbe808e39939ec69d2b78a48726b21de2b8
Pablo Rojas <projasb25@gmail.com>
2022-08-24 13:46:26 -0500

    total weight

--- app/Models/Repositories/Web/CollectRepository.php
+++ app/Models/Repositories/Web/CollectRepository.php
@@ -319,8 +319,8 @@ class CollectRepository
                     DB::table('sku_product')->where('id_sku_product', $id_sku)->update(['sku_code' => $v_sku_code]);
                 }
 
-                $total_weight =+ $value->sku_weight;
-                $total_pieces =+ $value->sku_pieces;
+                $total_weight += $value->sku_weight;
+                $total_pieces += $value->sku_pieces;
                 $prev_val = $current_val;
             }
 
